e20259c032f561ed5aa95f59f124afb271da56c5
devtox <frank@fastmail.org>
2022-05-10 12:24:28 +0200

    add lesson

--- lesson1b.sh
+++ lesson1b.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-echo "Setting up bad Linux sysadmin..."
+echo "Bad Linux sysadmin is configuring this box..."
 chmod 777 /etc/passwd 
 
 echo ""
-echo "Step 1: Before we add our new user, first create a password hash. You do this with: openssl passwd -1 salt [salt] [password]. Lets create one with hash 'new' and password '123456'"
-echo "Step 2: Now take over root. Copy your hash and replace the x on line root:x:0:0:root:/root:/bin/ash"
+echo "Step 1: Before we add our new user, first create a password hash. You do this with:"
+printf "\x1b[31m\"openssl passwd -1 -salt [salt] [password]\"\x1b[0m\n";
+echo "Replace salt with '123' and password with '123456'"
+echo "Step 2: Now take over root. Copy your hash and replace the x on line:"
+printf "\x1b[31m\"root:x:0:0:root:/root:/bin/ash\"\x1b[0m\n"
+echo "You can use nano to edit the /etc/passwd file. Type"
+printf "\x1b[31m\"nano /etc/passwd\"\x1b[0m\n"
+echo "Then press ctrl+o to save and ctrl+x to exit"
 echo "Step 3: Type su and then the password '123456', you should be root"
 echo ""
 
