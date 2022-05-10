1d9668952d92ea81f867cf5de76b9284f3c85401
devtox <frank@fastmail.org>
2022-05-10 12:16:23 +0200

    add lesson 1b

--- lesson1b.sh
+++ lesson1b.sh
@@ -3,6 +3,12 @@
 echo "Setting up bad Linux sysadmin..."
 chmod 777 /etc/passwd 
 
+echo ""
+echo "Step 1: Before we add our new user, first create a password hash. You do this with: openssl passwd -1 salt [salt] [password]. Lets create one with hash 'new' and password '123456'"
+echo "Step 2: Now take over root. Copy your hash and replace the x on line root:x:0:0:root:/root:/bin/ash"
+echo "Step 3: Type su and then the password '123456', you should be root"
+echo ""
+
 echo "Changing to user1..."
 adduser -S user1 -u 321 -D -s /bin/bash
 echo user1:password | chpasswd
@@ -11,8 +17,4 @@ clear
 cd $HOME 
 whoami
 
-echo "Ready!"
-echo "Step 1: Before we add our new user, first create a password hash. You do this with: openssl passwd -1 salt [salt] [password]. Lets create one with hash 'new' and password '123456'"
-echo "Step 2: Now take over root. Copy your hash and replace the x on line root:x:0:0:root:/root:/bin/ash"
-echo "Step 3: Type su and then the password '123456', you should be root"
  
